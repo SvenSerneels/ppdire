c8658efa6d6e166ad9d25d26fa770758da5a1e58
Sven Serneels <svenserneels@gmail.com>
2019-08-05 17:45:26 -0400

    Reducing dimensions

--- src/ppdire/__init__.py
+++ src/ppdire/__init__.py
@@ -9,8 +9,8 @@ Created on Wed Jul 9 14:20:17 2019
 __name__ = "ppdire"
 __author__ = "Sven Serneels"
 __license__ = "GNU"
-__version__ = "0.0.1"
-__date__ = "2019-07-09"
+__version__ = "0.0.2"
+__date__ = "2019-08-05"
 
 from .dicomo import dicomo
 from .capi import capi
