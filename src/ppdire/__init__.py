1c32817ebb7cef0700faf20973c943e694eb658b
Sven Serneels <svenserneels@gmail.com>
2020-04-06 15:28:11 -0400

    Farewell, ppdire!

--- src/ppdire/__init__.py
+++ src/ppdire/__init__.py
@@ -9,8 +9,8 @@ Created on Wed Jul 9 14:20:17 2019
 __name__ = "ppdire"
 __author__ = "Sven Serneels"
 __license__ = "GNU"
-__version__ = "0.2.2"
-__date__ = "2020-03-29"
+__version__ = "0.2.3"
+__date__ = "2020-04-06"
 
 from .dicomo import dicomo
 from .capi import capi
